e8365bd7a278b85f5444675dca036c39243e5c05
MikhailN45 <53788038+MikhailN45@users.noreply.github.com>
2023-02-06 15:10:35 +0300

    task 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
+    print("Type /start")
 
+    do {
+        val inputText = readLine()
+        if (inputText == "/start") {
+            println("App started, hello!")
+        }
+        else if (inputText == "/help") {
+            println("Available commands: /start for begin work, /help for info, /end for close app")
+        }
+        else if (inputText == "/end") {
+            println("App closed, good bye!")
+            break
+        } else println("Invalid input! Type /help for info")
+
+    } while (true)
 }
